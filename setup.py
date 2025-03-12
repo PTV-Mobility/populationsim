e97ac003a51fd6720af4c68d03c63d035b6bf8d8
MSnPTV <102351036+MSnPTV@users.noreply.github.com>
2025-03-12 14:57:42 +0100

    Update setup.py
    fixing the requirements to a stable combination

--- setup.py
+++ setup.py
@@ -19,9 +19,9 @@ setup(
     packages=find_packages(exclude=['*.tests']),
     include_package_data=True,
     install_requires=[
-        'activitysim >= 0.9.9.1',
-        'numpy >= 1.16.1',
-        'pandas >= 1.1.0',
-        'ortools >= 5.1.4045'
+        'activitysim = 1.1.0',
+        'numpy = 1.21.0',
+        'pandas = 1.5.2',
+        'ortools = 9.5.2237'
     ]
 )
